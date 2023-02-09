45f49d0270f8a1e9515b27603f2c03a419f04b23
StevenTian0 <zhenyuan.tian@mail.mcgill.ca>
2023-02-08 21:44:35 -0500

    Implement model layer
    Co-Authored-By: Steven Tian <90989589+StevenTian0@users.noreply.github.com>

---
+++ backend/src/server/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// Define the interface for the User document
+export interface IUser extends Document {
+  email: string;
+  username: string;
+  password: string;
+  languagePref: LanguagePref;
+  uiColor: UiColor;
+  score: number;
+}
+
+// Define the UI color enum type
+enum UiColor {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+// Define the language preference enum type
+enum LanguagePref {
+  EN = "EN",
+  FR = "FR",
+}
+
+// Define the Mongoose schema for the User document
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true, unique: true }, // email is a required string field and must be unique
+  username: { type: String, required: true, unique: true }, // username is a required string field and must be unique
+  password: { type: String, required: true }, // password is a required string field
+  languagePref: { type: LanguagePref, default: LanguagePref.EN }, // languagePref is an optional field, defaulting to EN
+  uiColor: { type: UiColor, default: UiColor.LIGHT }, // uiColor is an optional field, defaulting to LIGHT
+  score: { type: Number, default: 0 }, // score is an optional number field, defaulting to 0
+});
+
+// Compile the User model from the UserSchema and export it
+export default mongoose.model<IUser>("User", UserSchema);
