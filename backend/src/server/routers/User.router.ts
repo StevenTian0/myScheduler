154908b6e2e67d6952b5c391bbd0dc0e73934b80
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-02-11 01:32:55 -0500

    Update User.router.ts

--- backend/src/server/routers/User.router.ts
+++ backend/src/server/routers/User.router.ts
@@ -4,8 +4,8 @@ import {
   signUpController,
   loginController,
   updateUserController,
+  toggleColorUserController,
 } from "../controllers/User.controller";
-import { toggleColor } from "../services/User.service";
 
 const userRouter = Router();
 
@@ -13,6 +13,6 @@ userRouter.post("/api/user/signUp", signUpController);
 userRouter.delete("/api/user/delete", deleteUserAccount);
 userRouter.post("/api/user/login", loginController);
 userRouter.patch("/api/user/update", updateUserController);
-userRouter.patch("/api/user/toggleColor", toggleColor);
+userRouter.patch("/api/user/toggleColor", toggleColorUserController);
 
 export default userRouter;
