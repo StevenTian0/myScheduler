4bb02c6dbccf56e1792cc10c00f12fa641d974e4
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-02-10 23:07:52 -0500

    Update user model with enum types

--- backend/src/server/models/User.model.ts
+++ backend/src/server/models/User.model.ts
@@ -6,30 +6,40 @@ export interface IUser extends Document {
   email: string;
   username: string;
   password: string;
-  // languagePref: LanguagePref;
-  // uiColor: UiColor;
+  uiColor: UiColor;
+  languagePref: LanguagePref;
   score: number;
 }
 
-// // Define the UI color enum type
-// enum UiColor {
-//   LIGHT = "light",
-//   DARK = "dark",
-// }
+// Define the UI color enum type
+export enum UiColor {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
-// // Define the language preference enum type
-// enum LanguagePref {
-//   EN = "EN",
-//   FR = "FR",
-// }
+// Define the language preference enum type
+export enum LanguagePref {
+  EN = "EN",
+  FR = "FR",
+}
 
 // Define the Mongoose schema for the User document
 const UserSchema: Schema = new Schema({
   email: { type: String, required: true, unique: true }, // email is a required string field and must be unique
   username: { type: String, required: true, unique: true }, // username is a required string field and must be unique
   password: { type: String, required: true }, // password is a required string field
-  // languagePref: { type: LanguagePref, default: LanguagePref.EN }, // languagePref is an optional field, defaulting to EN
-  // uiColor: { type: UiColor, default: UiColor.LIGHT }, // uiColor is an optional field, defaulting to LIGHT
+  uiColor: {
+    // uiColor is an optional field, defaulting to LIGHT
+    type: String,
+    enum: Object.values(UiColor),
+    default: UiColor.LIGHT,
+  },
+  languagePref: {
+    // languagePref is an optional field, defaulting to EN
+    type: String,
+    enum: Object.values(LanguagePref),
+    default: LanguagePref.EN,
+  },
   score: { type: Number, default: 0 }, // score is an optional number field, defaulting to 0
 });
 
