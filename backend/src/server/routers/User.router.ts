6951c0c5d076f5b1f66aa95d1163f3e87b874ccd
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-02-11 01:29:15 -0500

    Add toggleColor router

--- backend/src/server/routers/User.router.ts
+++ backend/src/server/routers/User.router.ts
@@ -5,6 +5,7 @@ import {
   loginController,
   updateUserController,
 } from "../controllers/User.controller";
+import { toggleColor } from "../services/User.service";
 
 const userRouter = Router();
 
@@ -12,5 +13,6 @@ userRouter.post("/api/user/signUp", signUpController);
 userRouter.delete("/api/user/delete", deleteUserAccount);
 userRouter.post("/api/user/login", loginController);
 userRouter.patch("/api/user/update", updateUserController);
+userRouter.patch("/api/user/toggleColor", toggleColor);
 
 export default userRouter;
