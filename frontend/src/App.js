804674b1641e64176cd1f97c9568481115219d1f
qinghanz <qinghan.zhang@mail.mcgill.ca>
2023-03-25 12:43:13 -0400

    remove unecessary component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Calendar from "./Calendar";
 import Signup from "./Signup";
 import EditProfileForm from "./EditProfileForm";
 import Login from "./Login";
-import DeleteAccount from "./DeleteAccount";
 function App() {
   //notes is a list of note components
   return (
