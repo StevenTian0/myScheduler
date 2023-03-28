2f4e5ad3ad8233ed3d1dea4914b32892f5508e1b
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-03-27 22:40:28 -0400

    Implement Task Delete All

--- backend/src/server/routers/Task.router.ts
+++ backend/src/server/routers/Task.router.ts
@@ -5,6 +5,7 @@ import {
 	deleteController,
 	getController,
 	getAllController,
+	deleteAllController,
 } from "../controllers/Task.controller"
 
 const taskRouter = Router()
@@ -12,6 +13,7 @@ const taskRouter = Router()
 taskRouter.post("/api/task/create", addController)
 taskRouter.patch("/api/task/update", updateController)
 taskRouter.delete("/api/task/delete/:taskId", deleteController)
+taskRouter.delete("/api/task/delete/:token", deleteAllController)
 taskRouter.get("/api/task/getById/:taskId", getController)
 taskRouter.get("/api/task/getAll/:token", getAllController)
 
